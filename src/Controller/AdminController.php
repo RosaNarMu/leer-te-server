f4ba6b99db99fa13dd55cd2b3e45629b4bdfc40c
RosaNM <rosanmn.n@gmail.com>
2022-03-10 14:12:41 +0100

    AdminController get and delete implemented

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -18,7 +18,9 @@ use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 use App\Entity\Entrada;
 use App\Repository\CategoriaRepository;
+use App\Repository\CommentRepository;
 use App\Repository\EntradaRepository;
+use App\Repository\StoryRepository;
 use DateTime;
 use DateTimeInterface;
 use DateTimeZone;
@@ -34,11 +36,15 @@ class AdminController extends AbstractController
 
     private $em;
     private $userRepository;
+    private $storyRepository;
+    private $commentRepository;
 
-    public function __construct(EntityManagerInterface $em, UserRepository $userRepository)
+    public function __construct(EntityManagerInterface $em, UserRepository $userRepository, StoryRepository $storyRepository, CommentRepository $commentRepository)
     {
         $this->em = $em;
         $this->userRepository = $userRepository;
+        $this->storyRepository = $storyRepository;
+        $this->commentRepository = $commentRepository;
     }
 
     /**
@@ -76,5 +82,110 @@ class AdminController extends AbstractController
 
             ]
         );
+    }
+
+    /**
+     * @Route("/dataStory", name="data_story_admin", methods={"GET"})
+     */
+    public function adminStory(StoryRepository $storyRepository): Response
+    {
+        $story = $this->storyRepository->findBy(['published' => 1], []);
+
+        $result = [];
+
+        foreach ($story as $story) {
+            $result[] = [
+                'id' => $story->getId(),
+                'title' => $story->getTitle(),
+                'content' => $story->getContent(),
+                'publicationDate' => $story->getPublicationDate(),
+                'User' => $story->getUser()->getNickName(),
+                'genre' => $story->getGenre(),
+                'published' => $story->getPublished()
+            ];
+        }
+
+        return new JsonResponse($result);
+    }
+
+
+    /**
+     * @Route("/dataUser", name="data_user_admin", methods={"GET"})
+     */
+    public function adminUser(): Response
+    {
+        $user = $this->userRepository->findBy([], [/* 'nickName' => 'ASC' */]);
+
+        $result = [];
+
+        foreach ($user as $user) {
+            $result[] = [
+                'id' => $user->getId(),
+                'nickName' => $user->getNickName(),
+                'email' => $user->getEmail(),
+                'GoodReads' => $user->getGoodReads()
+            ];
+        }
+
+        return new JsonResponse($result);
+    }
+
+    /**
+     * @Route("/dataComment", name="data_comment_admin", methods={"GET"})
+     */
+    public function adminComment(): Response
+    {
+        $comment = $this->commentRepository->findBy([], []);
+
+        $result = [];
+
+        foreach ($comment as $comment) {
+            $result[] = [
+                'id' => $comment->getId(),
+                'content' => $comment->getContent(),
+                'publicationDate' => $comment->getPublicationDate(),
+                'score' => $comment->getScore(),
+                'User' => $comment->getUser()->getNickName(),
+                'Story ' => $comment->getStory()->getTitle(),
+            ];
+        }
+
+        return new JsonResponse($result);
+    }
+
+    /**
+     * @Route("/deleteStory/{id}", name="delete_story_admin", methods={"DELETE"})
+     */
+
+    public function deleteStory($id): Response
+    {
+        $story = $this->storyRepository->find($id);
+        $this->em->remove($story);
+        $this->em->flush();
+        return new JsonResponse(['respuesta' => 'ok']);
+    }
+
+    /**
+     * @Route("/deleteUser/{id}", name="delete_user_admin", methods={"DELETE"})
+     */
+
+    public function deleteUser($id): Response
+    {
+        $user = $this->userRepository->find($id);
+        $this->em->remove($user);
+        $this->em->flush();
+        return new JsonResponse(['respuesta' => 'ok']);
+    }
+
+    /**
+     * @Route("/deleteComment/{id}", name="delete_comment_admin", methods={"DELETE"})
+     */
+
+    public function deleteComment($id): Response
+    {
+        $comment = $this->commentRepository->find($id);
+        $this->em->remove($comment);
+        $this->em->flush();
+        return new JsonResponse(['respuesta' => 'ok']);
     }
 }
